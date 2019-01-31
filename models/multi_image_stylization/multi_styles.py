3fcfc65cf534ad057b1622cf367c1bb50c9fb611
Alexander Gunin <guninalexander@gmail.com>
2019-01-31 17:22:42 +0300

    test hooks

--- models/multi_image_stylization/multi_styles.py
+++ models/multi_image_stylization/multi_styles.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 import scipy.io
 import models.multi_image_stylization.vgg19 as vgg
 import tensorflow as tf
-from mbr.report.tensorflow_rpt import MlBoardReporter
+from mlboardclient.report.tensorflow_rpt import MlBoardReporter
 
 import models.multi_image_stylization.layers as layers
 
