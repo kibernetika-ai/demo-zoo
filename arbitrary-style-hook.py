dde6557ed327c81ad4091aa8e07f0c686b897974
Alexander Gunin <guninalexander@gmail.com>
2019-02-04 23:28:24 +0300

    fix export

--- arbitrary-style-hook.py
+++ arbitrary-style-hook.py
@@ -9,14 +9,14 @@ LOG = logging.getLogger(__name__)
 
 
 
-max_size = 1024
+max_size = 256
 styles = {}
 
 shornames = {'mosaic': 'mosaic', 'picasso': 'picasso_selfport1907', 'robert': 'robert', 'candy': 'candy', 'scream': 'the_scream', 'composition': 'composition_vii', 'shipwreck': 'shipwreck', 'rain': 'rain_princess', 'mosaic1': 'mosaic_ducks_massimo', 'escher': 'escher_sphere', 'udnie': 'udnie', 'wave': 'wave', 'woman': 'woman-with-hat-matisse', 'la_muse': 'la_muse', 'seated': 'seated-nude', 'pencil': 'pencil', 'strip': 'strip', 'feathers': 'feathers', 'starry': 'starry_night', 'stars2': 'stars2', 'frida': 'frida_kahlo'}
 
 
 PARAMS = {
-    'max_size': '512',
+    'max_size': '256',
     'styles_path': './data/styles'
 }
 
@@ -25,12 +25,12 @@ def init_hook(**params):
     global PARAMS
     PARAMS.update(params)
     global max_size
-    max_size = PARAMS.get('max_size', '1024')
+    max_size = PARAMS.get('max_size', '256')
     max_size = int(max_size)
     LOG.info('Max size {}'.format(max_size))
     for m in glob.glob(PARAMS['styles_path'] + '/*.jpg'):
         img = Image.open(m)
-        style = tensor_load_rgbimage(img, size=max_size)
+        style = tensor_load_rgbimage(img,max_size)
         global styles
         f = os.path.basename(m)
         f = f.split('.')[0]
@@ -55,13 +55,13 @@ def preprocess(inputs, ctx):
     h = int(h)
     w = int(w)
     ctx.original_w = w
-    ctx.original_h = w
+    ctx.original_h = h
 
     style = inputs.get('style', ['candy'])[0].decode("utf-8")
     style = shornames.get(style,style)
     style = styles[style]
 
-    content_image = tensor_load_rgbimage(content_image, size=max_size, keep_asp=True)
+    content_image = tensor_load_rgbimage(content_image,max_size)
 
     return {'content_inputs': [content_image],
             'style_inputs': [style]}
@@ -70,13 +70,18 @@ def preprocess(inputs, ctx):
 def postprocess(outputs, ctx):
     image = outputs['output'][0]
     image_bytes = io.BytesIO()
-    image = Image.fromarray(np.uint8(image))
+    image = Image.fromarray(np.uint8(image*255))
     image = image.resize((int(ctx.original_w),int(ctx.original_h)),Image.BILINEAR)
     image.save(image_bytes, format='JPEG')
     return {'output': image_bytes.getvalue()}
 
 
-def tensor_load_rgbimage(img, size=None, scale=None, keep_asp=False):
+def tensor_load_rgbimage(img,size):
+    img = img.convert('RGB')
+    img = img.resize((int(size), int(size)),Image.BILINEAR)
+    return np.asarray(img,np.float32)/255.0
+
+def tensor_load_rgbimage1(img, size=None, scale=None, keep_asp=False):
     img = img.convert('RGB')
     if size is not None:
         if keep_asp:
