f98e2ace58992a9db31dd10ad4107e635f044b2d
Alex <agunin@kibernetika.ai>
2020-01-14 16:25:40 -0800

    send cloud status

--- hooks/local.sh
+++ hooks/local.sh
@@ -31,4 +31,5 @@ kuberlab-serving --driver null \
   -o output_view=s \
   -o transfer_mode=direct $SERVING_MODELS \
   --params=cloud_status=1 \
+  --status_name=output \
   --cloud_config=126b6f0a-076a-4a97-b62e-16782bca69de@https://dev.kibernetika.io/api/v0.2/workspace/expo-recall/serving/xavier/config/live
