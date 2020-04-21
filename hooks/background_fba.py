7428624edc3781b34c90cfdd8fc284aa7fa8786d
Alex <agunin@kibernetika.ai>
2020-04-20 18:07:28 -0700

    new backgrounds

--- hooks/background_fba.py
+++ hooks/background_fba.py
@@ -185,8 +185,8 @@ def process(inputs, ct_x, **kwargs):
     #mask = cv2.resize(mask, (image.shape[1], image.shape[0]))
     mask[np.less_equal(mask, pixel_threshold)] = 0
     mask[np.greater(mask, pixel_threshold)] = 255
-    #mask = fba.pred(ct_x.global_ctx['fba'], image / 255, mask)
-    mask = mask.astype(np.float32)/255
+    mask = fba.pred(ct_x.global_ctx['fba'], image / 255, mask)
+    #mask = mask.astype(np.float32)/255
     #mask = cv2.resize(mask, (box[3] - box[1], box[2] - box[0]))
     #mask = np.pad(mask,
     #              ((box[0], process_height - box[2]), (box[1], process_width - box[3])),
