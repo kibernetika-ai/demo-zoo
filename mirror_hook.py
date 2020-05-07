21da6a37ad09430bbfb76157846af42c587eaf2f
Alex <agunin@kibernetika.ai>
2020-05-06 18:58:48 -0700

    fix mirror hook

--- mirror_hook.py
+++ mirror_hook.py
@@ -1,6 +1,7 @@
 import logging
 import cv2
 import numpy as np
+from ml_serving.utils.helpers import load_image
 
 LOG = logging.getLogger(__name__)
 
@@ -10,23 +11,10 @@ def init_hook(**kwargs):
 
 
 def process(inputs, ctx, **kwargs):
-    image = inputs.get('input')
-    if image is None:
-        raise RuntimeError('Missing input key in inputs. Provide an image in "input" key')
-    videoInput = True
-    if len(image.shape) < 3:
-        ##Encoded image
-        image = cv2.imdecode(np.frombuffer(image[0], np.uint8), cv2.IMREAD_COLOR)
-        image = image[:, :, ::-1]
-        videoInput = False
-
-    x = int(image.shape[1] / 2)
-    y = int(image.shape[0] / 2)
-    cv2.putText(image, 'Powered by Kibernetika.AI', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 4, (255, 255, 255),
-                        thickness=2, lineType=cv2.LINE_AA)
-    if videoInput:
-        return {'output': image}
+    original_image, is_video = load_image(inputs, 'inputs')
+    if is_video:
+        return {'output': original_image}
     else:
-        _, buf = cv2.imencode('.png', image[:, :, ::-1])
+        _, buf = cv2.imencode('.png', original_image[:, :, ::-1])
         image = np.array(buf).tostring()
         return {'output': image}
