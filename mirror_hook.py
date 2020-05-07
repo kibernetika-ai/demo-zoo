e3ff6010dc2636a44beff2a0333270a824996ed0
Alex <agunin@kibernetika.ai>
2020-05-06 21:20:27 -0700

    fix mirror hook

--- mirror_hook.py
+++ mirror_hook.py
@@ -13,7 +13,7 @@ def init_hook(**kwargs):
 def process(inputs, ctx, **kwargs):
     original_image, is_video = load_image(inputs, 'inputs')
     if is_video:
-        return {'outputs': original_image}
+        return {'outputs': original_image,'status': cv2.resize(original_image, (original_image.shape[1]//2, original_image.shape[0]//2))}
     else:
         _, buf = cv2.imencode('.png', original_image[:, :, ::-1])
         image = np.array(buf).tostring()
